d619223889f6fa40c2a7032b30f5c7652cd13f52
Yifeng Chu <cyanife@gmail.com>
2020-05-02 21:54:27 +0900

    arch

--- Arch/bootstrap.sh
+++ Arch/bootstrap.sh
@@ -1,29 +1,25 @@
 # during provisioning ~ or $HOME seems to be /root so we need to fix that
-export HOME=`/home/vagrant`
+export HOMEPATH='/home/vagrant'
 pacman -Syu --noconfirm
 
 # Installs
 pacman -S --noconfirm base-devel libutil-linux 
-
-pacman -S --noconfirm tmux git vim
-pacman -S --noconfirm lua subversion unzip
-pacman -S --noconfirm python-pip
+pacman -S --noconfirm tmux vim
+pacman -S --noconfirm git lua subversion unzip
+# pacman -S --noconfirm python-pip
 
 pacman -S --noconfirm zsh 
+chsh -s /bin/zsh vagrant
 
 # my dotfile setup
-if [ ! -d ~/dotfiles ]; then
-  git clone https://github.com/cyanife/dotfiles.git ~/dotfiles
-  chown -R vagrant:vagrant ~/dotfiles
+if [ ! -d $HOMEPATH/dotfiles ]; then
+  su -c "git clone https://github.com/cyanife/dotfiles.git $HOMEPATH/dotfiles" vagrant
+  su -s "/bin/zsh" -c "$HOMEPATH/dotfiles/zsh/bootstrap.sh" vagrant
 
-  zsh ~/dotfiles/zsh/bootstrap.sh
-
-  ln -s ~/dotfiles/zsh/.zshrc.local ~/.zshrc.local
-  touch ~/.zshrc
-  touch ~/.zlogout
-  echo "[ -f ~/.zshrc.local ] && source ~/.zshrc.local" >> ~/.zshrc
-  echo 'print -P "%F{33}▓▒░ %F{160}Exiting...%f"' >> ~/.zlogout
-  echo 'print -P "%F{33}▓▒░ %F{220}type %F{82}'h' %F{220}for vagrant command help%f"' >> ~/.zlogout
-  mkdir ~/share
-  chown vagrant:vagrant share/
+  ln -s $HOMEPATH/dotfiles/zsh/.zshrc.local $HOMEPATH/.zshrc.local
+  su -c "touch $HOMEPATH/.zshrc" vagrant
+  su -c "touch $HOMEPATH/.zlogout" vagrant
+  echo "[ -f ~/.zshrc.local ] && source ~/.zshrc.local" >> $HOMEPATH/.zshrc
+  echo 'print -P "%F{33}▓▒░ %F{160}Exiting...%f"' >> $HOMEPATH/.zlogout
+  echo 'print -P "%F{33}▓▒░ %F{220}type %F{82}'h' %F{220}for vagrant command help%f"' >> $HOMEPATH/.zlogout
 fi
--- Arch/connect.bat
+++ Arch/connect.bat
@@ -2,6 +2,10 @@
 @cd /d %~dp0
 
 @setlocal enableDelayedExpansion
+
+@set COLD_STATUS=("not created", "poweroff", "aborted")
+@set COOL_STATUS=("saved")
+
 @doskey vpu=vagrant snapshot push
 @doskey vpo=vagrant snapshot pop 
 @doskey vrl=vagrant reload $T vagrant ssh
@@ -15,7 +19,6 @@
 for /f "tokens=2* delims=," %%F IN ('vagrant status --machine-readable ^| find /I "state-human-short"') DO (SET "STATE=%%F%%G")
 SET STATE=%STATE:~25%
 
-IF "%STATE%"=="poweroff" (vagrant up) 
-IF "%STATE%"=="aborted" (vagrant up) 
-IF "%STATE%"=="saved" (vagrant resume)
+FOR %%s in %COLD_STATUS% do (IF "%STATE%"==%%s (vagrant up)) 
+FOR %%s in %COOL_STATUS% do (IF "%STATE%"==%%s (vagrant resume)) 
 vagrant ssh
